f703f01b91a1e381449531cee646c116c33ca9ff
mosaicthej <mij623@usask.ca>
2024-02-11 01:34:38 -0600

    preorder

--- a2/q2.lisp
+++ a2/q2.lisp
@@ -22,6 +22,39 @@
 )
 
 
+; They are all DFS. Need a stack
+; preorder:
+;  Visit the current node (in the figure: position red).
+;  Recursively traverse the current node's left subtree.
+;  Recursively traverse the current node's right subtree.
+; so... visit current, put left on stack, then put right
+(defun preOrder (Tree)
+  ; [left node right]
+  ; left-> car; node -> cadr ; right -> caddr
+  (defun preOrderProc (visited current stack)
+    (if (null (cadr current)) visited ; base case
+      (let ( (noLeft (null (car current)))
+             (noRight (null (caddr current)))  )
+        (cond 
+          ((and noLeft noRight) ;case-> leaf node.
+            (preOrderProc  ; pop stack to be next to visit.
+              (cons (cadr current) visited) ; visit node
+              (car stack) ; pop top of stack
+              (cdr stack)))
+          (noLeft (preOrderProc ; case -> missing L: move R. Stack unchanged
+              (cons (cadr current) visited) ;visit node
+              (caddr current) ; move to right, no change to stack
+              stack))
+          (noRight (preOrderProc; case -> missing R: move L. Stack unchanged
+              (cons (cadr current) visited) ; visit
+              (car current) ; move to left. no change on stack
+              stack))
+          (t (preOrderProc ; last case with both childs
+              (cons (cadr current) visited)
+              (car current) ; move to left, push right to stack
+              (cons (caddr current) stack)))))))
+  (rev(preOrderProc nil Tree nil) )); initialize
+
 
 ; (b) [5 Points]. 
 ; A polymorphic function, search, 
