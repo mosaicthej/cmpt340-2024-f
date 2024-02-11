846728e19e1ae94749ae7234b4fa5f3fd2307bb6
mosaicthej <mij623@usask.ca>
2024-02-10 23:47:17 -0600

    add q2 spec

---
+++ a2/q2.lisp
@@ -0,0 +1,33 @@
+; Problem 2 [35 Points]. 
+; Define a polymorphic Tree data type, 
+; where a Tree is either a Node with a left subtree, 
+;   a value, and a right subtree; or it is Null. 
+;
+; Also define a companion object for the Tree data type, 
+; with the following functions:
+;
+; (a) [15 Points]. 
+; Three polymorphic functions --  
+; inOrder, preOrder, and postOrder -- 
+;   to traverse the tree in-order, pre-order, and post-order.  
+; Each function takes a tree and returns a list with the 
+;   contents of the tree when traversed in that order.
+;
+
+; (b) [5 Points]. 
+; A polymorphic function, search, 
+; which takes two arguments 
+; — a Tree and a key — and returns a boolean result
+; based on whether the key is found in the tree.
+
+
+; (c) [5 Points]. 
+; A polymorphic function, replace, 
+; which takes three arguments 
+; — a Tree t, a value before, and a value after 
+; — and returns t with ALL instances of 
+;     before replaced with the value after.
+
+; [Hint: Model your Tree data type after the List data type presented in class.]
+
+ 
