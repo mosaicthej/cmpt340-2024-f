ab2d5de33bea635262f4d1f7f1db022c600caf57
Mark Jia <mjia@ualberta.ca>
2024-03-03 20:58:39 -0600

    q2 start

---
+++ a3/q2.scala
@@ -0,0 +1,30 @@
+package q2
+
+import scala.util.Success
+/* Problem 2 [25 Points]. 
+  Recall that unlike Options 
+  where multiple errors are simply subsumed by a None, 
+  when we deal with multiple errors in Eithers, 
+    all but one are lost.  
+  
+  Implement a datatype to return back multiple errors.   
+  
+  Pay close attention to situations which do and 
+    do not require recording of multiple errors.  
+
+  You may use the following definition 
+  for the purpose of defining your data type. */
+
+/* Now, implement the following functions for this datatype.
+accumulating errors when meaningful to do so. */
+
+/*
+  Per my design, map3, map2, and sequence are
+  the only ones that errors can be accumulated.
+*/
+
+sealed class Partial[+E, +A] {
+case class Errors[+E](get: Seq[E]) extends Partial[E, Nothing] {
+}
+case class Success[+A](get: A) extends Partial[Nothing, A] {
+}
